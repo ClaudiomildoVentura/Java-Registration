ca3f4d2d191e2a2f07db5300fe9b0a43223615a7
Claudiomildo <claudiomildo@gmail.com>
2020-01-19 20:04:33 -0300

    Ajustes no GeneralController

--- src/main/java/com/app/crud/models/UserModels.java
+++ src/main/java/com/app/crud/models/UserModels.java
@@ -22,7 +22,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
-@Table(name="app_user")
+@Table(name="user")
 public class UserModels implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
