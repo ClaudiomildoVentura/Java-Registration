a0b2a1bb1f77f64e5e11baf204e1e31a80bee867
Claudiomildo <claudiomildo@gmail.com>
2020-01-19 17:04:43 -0300

    Refatoração no arquivo app/controllers

--- src/main/resources/static/app/controllers/access-denied.js
+++ src/main/resources/static/app/controllers/access-denied.js
@@ -1,4 +1,4 @@
 angular.module('AddressBook')
-// Creating the Angular Controller
-.controller('AccessDeniedController', function($http, $scope, AuthService) {
-});
+    // Creating the Angular Controller
+    .controller('AccessDeniedController', function ($http, $scope, AuthService) {
+    });
