54f0309b26c242e9f0df743c3e6e6f15fed15c4d
Claudiomildo <claudiomildo@gmail.com>
2020-01-19 16:48:18 -0300

    Refatoração no arquivo AddressRepository

--- src/main/java/org/techforumist/addressbook/repository/AddressRepository.java
+++ src/main/java/org/techforumist/addressbook/repository/AddressRepository.java
@@ -3,10 +3,4 @@ package org.techforumist.addressbook.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.techforumist.addressbook.domain.Address;
 
-/**
- * @author Sarath Muraleedharan
- *
- */
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
+public interface AddressRepository extends JpaRepository<Address, Long> {}
