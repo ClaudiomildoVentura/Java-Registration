c4b06d0eb7e9156535e0c8dd570fe206f021a4c0
Claudiomildo <claudiomildo@gmail.com>
2020-01-21 19:35:57 -0300

    Refatoração

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into user(id_user, name_user, user_name, password,role) values (1,'John','admin','admin','ADMIN');
-insert into user(id_user, name_user, user_name, password,role) values (2,'Liza','user','user','USER');
+insert into user(id_user, name_user, user_name, password, role) values (1,'John','admin','admin','ADMIN');
+insert into user(id_user, name_user, user_name, password, role) values (2,'Liza','user','user','USER');
 
 insert into user_log(name,address,owner) values ('Jones','Administrative Officer',1);
 insert into user_log(name,address,owner) values ('Ervin','Counselor of Embassy for Administrative',1);
